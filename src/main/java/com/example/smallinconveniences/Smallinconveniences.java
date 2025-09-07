3c42930df7aad51157b6ec1f1e8ccd6585bd570f
Dylan Tseng <108163886+Deeeeelan@users.noreply.github.com>
2025-09-07 12:02:38 -0700

    Added model for test_item

--- src/main/java/com/example/smallinconveniences/Smallinconveniences.java
+++ src/main/java/com/example/smallinconveniences/Smallinconveniences.java
@@ -4,7 +4,10 @@ import net.fabricmc.api.ModInitializer;
 
 public class Smallinconveniences implements ModInitializer {
 
+    public static final String MOD_ID = "smallinconveniences";
+
     @Override
     public void onInitialize() {
+        ModItems.initialize();
     }
 }
