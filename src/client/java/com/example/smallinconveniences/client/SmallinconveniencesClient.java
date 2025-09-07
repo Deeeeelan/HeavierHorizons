a1402c4457c2a261463419640a0d0ef16175e348
Dylan Tseng <108163886+Deeeeelan@users.noreply.github.com>
2025-09-06 17:13:37 -0700

    Initial commit

---
+++ src/client/java/com/example/smallinconveniences/client/SmallinconveniencesClient.java
@@ -0,0 +1,10 @@
+package com.example.smallinconveniences.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SmallinconveniencesClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/com/example/smallinconveniences/Smallinconveniences.java
@@ -0,0 +1,10 @@
+package com.example.smallinconveniences;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Smallinconveniences implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
