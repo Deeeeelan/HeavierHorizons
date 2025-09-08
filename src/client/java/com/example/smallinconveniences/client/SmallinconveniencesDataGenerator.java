c90062dee68e442d04f5b6830f8fa5fbd8437a8e
Dylan Tseng <108163886+Deeeeelan@users.noreply.github.com>
2025-09-07 17:01:47 -0700

    Add steel block recipe

--- src/client/java/com/example/smallinconveniences/client/SmallinconveniencesDataGenerator.java
+++ src/client/java/com/example/smallinconveniences/client/SmallinconveniencesDataGenerator.java
@@ -1,5 +1,6 @@
 package com.example.smallinconveniences.client;
 
+import com.example.smallinconveniences.Smallinconveniences;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
@@ -8,5 +9,6 @@ public class SmallinconveniencesDataGenerator implements DataGeneratorEntrypoint
     @Override
     public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
         FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+        pack.addProvider(SmallInconveniencesRecipeProvider::new);
     }
 }
